17a78eaa2e1920f9a5c0d070eae2a26f70e2d6ff
emmanewkirk <emmanewkirk7@gmail.com>
2015-01-23 16:24:35 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+//Emma Newkirk  Lab 1  HelloWorld
+
+//class
+public class HelloWorld {
+    //main method
+    
+    public static void main (String[] args) {
+        
+        //print the statement
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        //print test
+        System.out.println("test");
+    }
+}
