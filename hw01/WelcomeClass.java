50ec837323238e6e6dc20cb176f7bda9c60b2027
emmanewkirk <emmanewkirk7@gmail.com>
2015-01-26 20:58:20 +0000

    Homework 1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Emma Newkirk  CSE2  23 Jan 2015
+
+//class
+public class WelcomeClass {
+    
+    //method
+    public static void main (String[] args) {
+        
+        //print welcome banner
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");        
+        
+        //print fancy signature
+        System.out.println("  ^   ^   ^   ^   ^   ^  ");
+        System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\ ");
+        System.out.println("<-E- -J- -N- -2- -1- -5->");
+        System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ / ");
+        System.out.println("  v   v   v   v   v   v  ");
+        
+        //print autobiography
+        System.out.println("I'm a senior neuroscience major, and no, I don't have any idea what I'm doing after graduation.");
+    }
+    
+    
+}
